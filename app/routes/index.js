02940c30fc93d67db46a7e6218f36b7c57069604
jolavb <julianb@bu.edu>
2018-01-18 16:07:32 -0500

    Add validation to new comment
    verify new comment is not empty in controller.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  classNames: ['test'],
   search: Ember.inject.service(),
   model() {
     return  this.get('search')
--- app/routes/study.js
+++ app/routes/study.js
@@ -2,13 +2,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  flashMessages: Ember.inject.service(),
   model (params) {
     return this.get('store').findRecord('study', params.study_id);
   },
   actions: {
     newComment(comment){
-      let newComment = this.get('store').createRecord('comment', comment);
-      newComment.save();
+      if(Ember.isEmpty(comment.text)) {
+        this.get('flashMessages').warning('You forgot to write anything!');
+      } else {
+        let newComment = this.get('store').createRecord('comment', comment);
+        newComment.save();
+      };
     },
     deleteComment(comment) {
       comment.destroyRecord();
