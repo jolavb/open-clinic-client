21fdadd4def698217bfb66420e9afd6e5813f47f
jolavb <julianb@bu.edu>
2018-01-18 11:12:57 -0500

    Add search component to index route.
    -Add PowerSelect to index route.
    -Link powerSelect options to search service.
    -Modify list-studies to bind to search service attributes.
    -Modify list-studies initialize action to search based on stored
    propertiess from search service.

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -1,19 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  search: Ember.inject.service(),
   spinner: Ember.inject.service(),
   classNames: ['list-filter'],
   selectedPhases: null,
   phasesOptions: ['Early Phase 1','Phase 1','Phase 1/Phase 2', 'Phase 2', 'Phase 2/Phase 3','Phase 3', 'Phase 4'],
-  value: '',
   init() {
     this._super(...arguments);
-    this.get('filter')('').then((results) => this.set('results', results));
+    this.get('filter')(this.get('search.selectedPhases')).then((results) => this.set('results', results));
   },
   actions: {
     searchStudies() {
       let spinner = this.get('spinner')
-      let filterInputValue = this.get('selectedPhases');
+      let filterInputValue = this.get('search.selectedPhases');
       let filterAction = this.get('filter');
       spinner.show('uniq-123')
       filterAction(filterInputValue)
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
+  search: Ember.inject.service(),
+  model() {
+    return  this.get('search')
+  },
+  actions: {
+    searchStudies() {
+      this.transitionTo('studies');
+    }
+  }
 });
