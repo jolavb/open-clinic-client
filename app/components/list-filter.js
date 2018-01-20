12a965a086d1f9930dd87a85d9558d62274297d1
jolavb <julianb@bu.edu>
2018-01-20 17:10:56 -0500

    Add title to search options
    1. Add input for title to list-filter
    2. Reformat bound input values into object in search service.

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -20,12 +20,21 @@ export default Ember.Component.extend({
   },
   actions: {
     searchStudies() {
+      // Get spinner service as spinner
       let spinner = this.get('spinner');
-      let filterInputValue = this.get('search.selectedPhases');
+      // get filterInputValue as search service selected phases
+      let filterInputValue = this.get('search.filter');
+      // get filterAction as filterByPhase which is defined in the studies controller.
       let filterAction = this.get('filter');
+      // show spinner
       spinner.show('uniq-123');
+      // call filter action, passing it the input values. The controller then
+      // returns the filtered results in a promise chain.
       filterAction(filterInputValue)
+      // set results = to output of filterAction. List-filter yeilds results which
+      // are then looped through in studies.hbs.
         .then((filterResults) => this.set('results', filterResults))
+        // hide filter
         .then(()=> spinner.hide('uniq-123'));
     }
   }
--- app/services/search.js
+++ app/services/search.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  filter: {
+      title: null,
+      selectedPhases: null
+  },
+  title: null,
   phaseOptions: ['Early Phase 1','Phase 1','Phase 1/Phase 2', 'Phase 2', 'Phase 2/Phase 3','Phase 3', 'Phase 4'],
-  selectedPhases: null,
+
 });
