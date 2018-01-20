c09c4507385774520f3fea65dc47854938bb426f
jolavb <julianb@bu.edu>
2018-01-20 17:10:56 -0500

    Add title to search options
    1. Add input for title to list-filter
    2. Reformat bound input values into object in search service.

--- app/controllers/studies.js
+++ app/controllers/studies.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  search: Ember.inject.service(),
+
   actions: {
     filterByPhase(param) {
       if (param !== '') {
-        return this.get('store').query('study', { phase: param });
+        return this.get('store').query('study', { phase: param.selectedPhases });
       } else {
         return this.get('store').findAll('study');
       }
--- app/routes/studies.js
+++ app/routes/studies.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
-
   model() {
     return RSVP.Promise.resolve({});
   }
