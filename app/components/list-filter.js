5dee709d6612decfa29afae4958b3e1e61e85d60
jolavb <julianb@bu.edu>
2018-01-20 16:20:27 -0500

    FIx ember syntax errors
    Add missing semi-colons

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -8,26 +8,25 @@ export default Ember.Component.extend({
   phasesOptions: ['Early Phase 1','Phase 1','Phase 1/Phase 2', 'Phase 2', 'Phase 2/Phase 3','Phase 3', 'Phase 4'],
   didInsertElement() {
     this._super(...arguments);
-    let params = ''
-    let spinner = this.get('spinner')
-    spinner.show('uniq-123')
+    let params = '';
+    let spinner = this.get('spinner');
+    spinner.show('uniq-123');
     if (this.get('search.selectedPhases')) {
-      params = this.get('search.selectedPhases')
+      params = this.get('search.selectedPhases');
     }
     this.get('filter')(params)
       .then((results) => this.set('results', results))
-      .then(()=> spinner.hide('uniq-123'))
+      .then(()=> spinner.hide('uniq-123'));
   },
   actions: {
     searchStudies() {
-      let spinner = this.get('spinner')
+      let spinner = this.get('spinner');
       let filterInputValue = this.get('search.selectedPhases');
       let filterAction = this.get('filter');
-      spinner.show('uniq-123')
+      spinner.show('uniq-123');
       filterAction(filterInputValue)
         .then((filterResults) => this.set('results', filterResults))
-        .then(()=> spinner.hide('uniq-123'))
-      ;
+        .then(()=> spinner.hide('uniq-123'));
     }
   }
 });
--- app/services/search.js
+++ app/services/search.js
@@ -3,6 +3,4 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   phaseOptions: ['Early Phase 1','Phase 1','Phase 1/Phase 2', 'Phase 2', 'Phase 2/Phase 3','Phase 3', 'Phase 4'],
   selectedPhases: null,
-  searhPhases(selectedPhases) {
-  }
 });
