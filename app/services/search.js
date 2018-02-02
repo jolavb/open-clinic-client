8f1be3b091c5ae3c7dca32c377c5cc054f84db6c
jolavb <julianb@bu.edu>
2018-02-02 13:27:58 -0500

    Filter params for studies request
    1. "cleaned" params sent to studies get action to include
    only parameters that are not null.

--- app/services/search.js
+++ app/services/search.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   filter: {
       officialTitle: null,
-      selectedPhases: null
+      selectedPhases: [],
   },
   title: null,
   phaseOptions: ['Early Phase 1','Phase 1','Phase 1/Phase 2', 'Phase 2', 'Phase 2/Phase 3','Phase 3', 'Phase 4'],
