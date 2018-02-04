59089c06df2404db5bc158c2d1f77c5ae3c9aace
jolavb <julianb@bu.edu>
2018-02-04 09:40:53 -0500

    Add Pagination to studies
    1. Add first, previous and next buttons tied to meta links
    data
    2. Generate array (nextTenPages) to return 10 pages from the current
    page and iterate through array in hbs file to display page link buttons
    3. tie page link buttons to page actions
    4. Add per page option to siplay varying number of results.

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -3,9 +3,23 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   search: Ember.inject.service(),
   spinner: Ember.inject.service(),
+  totalPages: Ember.computed('search.meta', function(){
+    return this.get('search.meta.total_pages');
+  }),
+  // Ember.computed('search.filter.per_page', function(){
+  //   return this.get('search.filter.per_page')
+  // }),
+
+  nextTenPages: Ember.computed('search.meta', function(){
+    let total = this.get('search.meta.total_pages');
+    let current = this.get('search.meta.current_page');
+    let pages = Array.from(new Array(total),(val,index)=>index);
+    return pages.slice(current, current + 10);
+  }),
   classNames: ['list-filter'],
   selectedPhases: null,
   phasesOptions: ['Early Phase 1','Phase 1','Phase 1/Phase 2', 'Phase 2', 'Phase 2/Phase 3','Phase 3', 'Phase 4'],
+
   didInsertElement() {
     this._super(...arguments);
     let params = '';
@@ -16,27 +30,55 @@ export default Ember.Component.extend({
     }
     // passes current search params to studies controller action
     this.get('filter')(params)
-      .then((results) => this.set('results', results))
+      .then((results) => {
+        this.set('search.meta', results.meta);
+        this.set('results', results);
+      })
       .then(()=> spinner.hide('uniq-123'));
   },
   actions: {
-    searchStudies() {
-      // Get spinner service as spinner
+    searchStudies(updated,showspin) {
+
+      if (updated) {
+        this.set('search.filter.page', 1);
+      }
+       // Get spinner service as spinner
       let spinner = this.get('spinner');
       // get filterInputValue as search service selected phases
       let filterInputValue = this.get('search.filter');
       // get filterAction as filterByPhase which is defined in the studies controller.
       let filterAction = this.get('filter');
       // show spinner
-      spinner.show('uniq-123');
+
+      if(showspin) {
+        spinner.show('uniq-123');
+      }
+      // spinner.show('uniq-123');
       // call filter action, passing it the input values. The controller then
       // returns the filtered results in a promise chain.
       filterAction(filterInputValue)
       // set results = to output of filterAction. List-filter yeilds results which
       // are then looped through in studies.hbs.
-        .then((filterResults) => this.set('results', filterResults))
-        // hide filter
+        .then((filterResults) => {
+          this.set('search.meta', filterResults.meta);
+          this.set('results', filterResults);
+        })
+        // hide spinner
         .then(()=> spinner.hide('uniq-123'));
+    },
+    next(){
+      let next = this.get('search.meta.next_page');
+      this.set('search.filter.page', next);
+      this.send('searchStudies');
+    },
+    previous(){
+      let prev = this.get('search.meta.prev_page');
+      this.set('search.filter.page', prev);
+      this.send('searchStudies');
+    },
+    pageLink(page){
+      this.set('search.filter.page', page);
+      this.send('searchStudies');
     }
   }
 });
--- app/services/search.js
+++ app/services/search.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  meta: {},
   filter: {
       officialTitle: null,
       selectedPhases: [],
+      page: 1,
+      per_page:10
   },
+
+  perPage: [10,25,50,100],
   title: null,
   phaseOptions: ['Early Phase 1','Phase 1','Phase 1/Phase 2', 'Phase 2', 'Phase 2/Phase 3','Phase 3', 'Phase 4'],
 
