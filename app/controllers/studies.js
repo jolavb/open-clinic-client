41ba10952efcaad12772c9e78e77871fa689c52f
jolavb <julianb@bu.edu>
2018-02-11 11:07:41 -0500

    Support multi phase search
    1. Add phase filter to studies route
    2. Add phase params to Studies controller

--- app/controllers/studies.js
+++ app/controllers/studies.js
@@ -1,34 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  search: Ember.inject.service(),
-  page: 1,
-
-  metaData: Ember.computed('model', function(){
-    let meta = this.get('model.meta');
-    meta['second_last'] = meta.total_pages - 1;
-    meta['third_last'] = meta.total_pages - 2;
-    return meta;
-  }),
-
-  lastThreePages: Ember.computed('model', function(){
-    if (this.get('page') < this.get('metaData.total_pages') - 3) {
-      return false;
-    } else {
-      return true;
-    }
-  }),
+  queryParams: ["page", "perPage", "officialTitle", "selectedPhases" ],
+  officialTitle: "All",
+  phaseOptions: ['Early Phase 1','Phase 1','Phase 1/Phase 2', 'Phase 2', 'Phase 2/Phase 3','Phase 3', 'Phase 4'],
+  selectedPhases: "All",
 
   actions: {
-    filterByPhase(param) {
-      let cleanedParams = {};
-      // updates params for get request to only include non-blank
-      Object.entries(param).forEach((entry) =>{
-        if (entry[1] && entry[1] !== 0) {
-            cleanedParams[entry[0]] = entry[1];
-        }
-      });
-      return this.get('store').query('study', cleanedParams)
-    },
-   },
+    searchStudies(title,selectedPhase) {
+      if (Ember.isEmpty(title)) {
+        title = "All"
+      }
+      if (Ember.isEmpty(selectedPhase)) {
+        selectedPhase = 'All'
+      }
+
+      this.setProperties({officialTitle: title, selectedPhases: selectedPhase})
+    }
+  }
 });
--- app/routes/studies.js
+++ app/routes/studies.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
-import RSVP from 'rsvp';
 
-export default Ember.Route.extend({
-  model() {
-    return RSVP.Promise.resolve({});
+import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
+
+export default Ember.Route.extend(RouteMixin,{
+  perPage: 25,
+  queryParams: {
+    officialTitle: { refreshModel: true },
+    selectedPhases: { refreshModel: true }
+  },
+  model(params) {
+    return this.findPaged('study',params)
   }
 
 });
